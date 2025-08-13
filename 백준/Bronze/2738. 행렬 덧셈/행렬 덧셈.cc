46064e76d003a8944393000bdc2e202e8d36a4c3
sonbuwon <84784246+sonbuwon@users.noreply.github.com>
2025-08-13 16:01:29 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int N, M;
+    cin >> N;
+    cin >> M;
+    
+    int arr1[N][M];
+    int arr2[N][M];
+    
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<M; j++) {
+            cin >> arr1[i][j];
+        }
+    }
+    
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<M; j++) {
+            cin >> arr2[i][j];
+        }
+    }
+    
+    for(int i=0; i<N; i++) {
+        for(int j=0; j<M; j++) {
+            cout << arr1[i][j] + arr2[i][j] << ' ';
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
