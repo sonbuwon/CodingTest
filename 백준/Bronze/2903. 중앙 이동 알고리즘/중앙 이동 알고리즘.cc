f6e25bb5cb9452fb253edfebf9490234e18b31a3
sonbuwon <84784246+sonbuwon@users.noreply.github.com>
2025-08-26 09:58:22 +0900

    [Bronze III] Title: 중앙 이동 알고리즘, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.cc
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.cc
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 
-int main() {
-    int n;
-    cin >> n;
+int main()
+{
+    int N;
+    cin >> N;
     
-    int side_points = pow(2, n) + 1;
+    int temp = pow(2, N) + 1;
     
-    int total_points = side_points * side_points;
-    
-    cout << total_points << endl;
+    cout << temp * temp << endl;
     
     return 0;
 }
