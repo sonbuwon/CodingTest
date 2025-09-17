747551fc8676f24bd4787eae4e46396f0392e65f
sonbuwon <84784246+sonbuwon@users.noreply.github.com>
2025-09-17 12:48:47 +0900

    [Bronze II] Title: 전구, Time: 1524 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/21918. 전구/전구.py
@@ -0,0 +1,23 @@
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+    
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    
+    if a == 1:
+        arr[b-1] = c
+    elif a == 2:
+        for i in range(b-1, c):
+            if arr[i] == 0:
+                arr[i] = 1
+            elif arr[i] == 1:
+                arr[i] = 0
+    elif a == 3:
+        for i in range(b-1, c):
+            arr[i] = 0
+    elif a == 4:
+        for i in range(b-1, c):
+            arr[i] = 1
+            
+for i in arr:
+    print(i, end=' ')
