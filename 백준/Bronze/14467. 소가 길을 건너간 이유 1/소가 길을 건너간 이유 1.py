bf9b665b69f45aa266c6c4afbe4cdbc76be3157e
sonbuwon <84784246+sonbuwon@users.noreply.github.com>
2025-10-27 14:27:05 +0900

    [Bronze I] Title: 소가 길을 건너간 이유 1, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.py
+++ 백준/Bronze/14467. 소가 길을 건너간 이유 1/소가 길을 건너간 이유 1.py
@@ -1,24 +1,16 @@
 N = int(input())
-records = []
-total_count = 0
+
+cow_info = {}
+count = 0
 
 for _ in range(N):
-    num, loc = map(int, input().split())
-    records.append([num, loc])
+    cow_num, cow_pos = map(int, input().split())
 
-cow_nums = set()
-for record in records:
-    cow_nums.add(record[0])
-    
-for cow_num in cow_nums:
-    isFirst=True
-    now_posi = []
-    for record in records:
-        if record[0] == cow_num and isFirst == True:
-            isFirst = False
-            now_posi = [record[0], record[1]]
-        if record[0] == cow_num and record[1] != now_posi[1] and isFirst == False:
-            total_count+=1
-            now_posi = [record[0], record[1]]
-            
-print(total_count)
+    if cow_num not in cow_info:
+        cow_info[cow_num] = cow_pos
+    else:
+        if cow_info[cow_num] != cow_pos:
+            count += 1
+            cow_info[cow_num] = cow_pos
+
+print(count)
